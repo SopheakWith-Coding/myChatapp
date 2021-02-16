d8acc52178196c30076bd5a8f37db4a864ef1a9c
“SopheakWith-Coding” <sopheak.seng@muuve.biz>
2021-02-16 11:56:14 +0700

    fixed_selected_multiple_user

--- src/screen/newGroup.js
+++ src/screen/newGroup.js
@@ -5,18 +5,17 @@ import {
   Text,
   Dimensions,
   View,
-  TouchableOpacity,
   Image,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
-
 const screenWidth = Dimensions.get('screen').width;
 export default class newGroupChatScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
+      selectedUser: [],
     };
   }
 
@@ -31,11 +30,42 @@ export default class newGroupChatScreen extends React.Component {
   };
 
   render() {
-    const {users} = this.state;
+    const {users, selectedUser} = this.state;
+    console.log(selectedUser);
     const {uid} = auth().currentUser;
     const filterUser = users.filter((val) => val.uuid !== uid);
     return (
       <View style={styles.container}>
+        <View style={styles.ShowUser}>
+          <FlatList
+            horizontal
+            data={selectedUser}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item, index}) => {
+              const image = item.profileImage
+                ? {uri: `${item.profileImage}`}
+                : {
+                    uri:
+                      'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?b=1&k=6&m=1214428300&s=612x612&w=0&h=kMXMpWVL6mkLu0TN-9MJcEUx1oSWgUq8-Ny6Wszv_ms=',
+                  };
+              return (
+                <View style={styles.HorizontalContainer}>
+                  <View style={styles.HorizontalSubContainer}>
+                    <View style={styles.HorizontalImageWrapper}>
+                      <Image
+                        style={{width: 50, height: 50, borderRadius: 50}}
+                        source={image}
+                      />
+                    </View>
+                    <View style={styles.HorizontalTextWrapper}>
+                      <Text style={{fontSize: 18}}>{item.name}</Text>
+                    </View>
+                  </View>
+                </View>
+              );
+            }}
+          />
+        </View>
         <FlatList
           data={filterUser}
           keyExtractor={(item, index) => index.toString()}
@@ -47,19 +77,18 @@ export default class newGroupChatScreen extends React.Component {
                     'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?b=1&k=6&m=1214428300&s=612x612&w=0&h=kMXMpWVL6mkLu0TN-9MJcEUx1oSWgUq8-Ny6Wszv_ms=',
                 };
             return (
-              <TouchableOpacity>
-                <View style={styles.SubContainer}>
-                  <View style={styles.imageWrapper}>
-                    <Image
-                      style={{width: 50, height: 50, borderRadius: 50}}
-                      source={image}
-                    />
-                  </View>
-                  <View style={styles.TextWrapper}>
-                    <Text style={styles.TextTitle}>{item.name}</Text>
-                  </View>
+              <View style={styles.SubContainer}>
+                <View style={styles.imageWrapper}>
+                  <Image
+                    style={{width: 50, height: 50, borderRadius: 50}}
+                    source={image}
+                  />
                 </View>
-              </TouchableOpacity>
+                <View style={styles.TextWrapper}>
+                  <Text style={styles.TextTitle}>{item.name}</Text>
+                </View>
+                <View style={styles.CheckBoxWrapper} />
+              </View>
             );
           }}
         />
@@ -88,11 +117,50 @@ const styles = StyleSheet.create({
   },
   TextWrapper: {
     marginVertical: 1,
-    marginRight: 15,
     justifyContent: 'center',
-    width: screenWidth - 90,
+    width: screenWidth / 2 + 70,
   },
   TextTitle: {
     fontSize: 18,
+  },
+  CheckBoxWrapper: {
+    marginVertical: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+    width: screenWidth / 12,
+  },
+  CheckBox: {
+    width: 25,
+    height: 25,
+  },
+  ShowUser: {
+    width: screenWidth,
+    height: 140,
+    backgroundColor: 'white',
+  },
+  HorizontalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  HorizontalSubContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  HorizontalImageWrapper: {
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+  },
+  HorizontalTextWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    marginVertical: 2,
   },
 });
